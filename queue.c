24403f24e8095a15bfd89939b4818e6cf668ec11
maroma samsa <y9740201@gmail.com>
2024-03-02 02:03:15 +0000

    Feat: q_insert_tail implementation
    this function insert new nodes with data value at tail of the current
    queue

--- queue.c
+++ queue.c
@@ -57,6 +57,15 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+
+    element_t *e = (element_t *) malloc(sizeof(element_t));
+    if (!e)
+        return false;
+
+    e->value = s;
+    list_add_tail(&e->list, head);
     return true;
 }
 
