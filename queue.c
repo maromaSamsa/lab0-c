605297a5a40fc1d2cef9fbdf815f25d45b66625a
maroma samsa <y9740201@gmail.com>
2024-03-02 01:59:52 +0000

    Feat: q_insert_head implementation
    this function insert an new element with data value at the begining of
    the current queue

--- queue.c
+++ queue.c
@@ -42,6 +42,15 @@ void q_free(struct list_head *head)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+
+    element_t *e = (element_t *) malloc(sizeof(element_t));
+    if (!e)
+        return false;
+
+    e->value = s;
+    list_add(&e->list, head);
     return true;
 }
 
